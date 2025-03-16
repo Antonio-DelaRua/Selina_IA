8e02a02b0893a50555e25cdea8b28967a577e482
RuXx <ruxxdeveloper@gmail.com>
2025-03-16 22:20:01 +0100

    prueba-git

--- alias_dic.py
+++ alias_dic.py
@@ -1,8 +1,48 @@
 predefined_answers = {
     "hola": "Pulsa ESC para destruir el mundo",
-    "entorno virtual": "Para crear un entorno virtual en Python:\n"
-                       "```\npython -m venv env\n\n```"
-                     
-                       "Para activarlo en CMD:\n"
-                       "```\nenv\\Scripts\\activate\n```"
+
+    "entorno virtual": (
+        "Para crear un entorno virtual en Python:\n"
+        "```\npython -m venv env\n```\n\n"
+        "Para activarlo en CMD:\n"
+        "```\nenv\\Scripts\\activate\n```"
+    ),
+    
+    "git": (
+        "Comandos básicos de Git\n\n"
+        "Inicializa un nuevo repositorio de Git en el directorio actual.\n"
+        "```\ngit init\n```\n\n"
+        "Clona un repositorio existente en una nueva carpeta\n"
+        "```\ngit clone <url-del-repositorio>\n```\n\n"
+        "Muestra el estado actual del repositorio, incluyendo archivos modificados, añadidos y eliminados.\n"
+        "```\ngit status\n```\n\n"
+        "Añade archivos al área de preparación (staging area).\n"
+        "```\ngit add <archivo>\n```\n"
+        "```\ngit add .\n```\n\n"
+        "Guarda los cambios en el historial del repositorio con un mensaje descriptivo.\n"
+        "```\ngit commit -m 'Mensaje del commit'\n```\n\n"
+        "Actualiza el repositorio local con los cambios del repositorio remoto.\n"
+        "```\ngit pull <nombre-remoto> <nombre-rama>\n```\n\n"
+        "Cambia a otra rama o restaura archivos en el directorio de trabajo.\n"
+        "```\ngit checkout <nombre-rama>  # Cambia a una rama existente\n```\n"
+        "```\ngit checkout -b <nombre-rama>  # Crea y cambia a una nueva rama\n```\n\n"
+        "Fusiona cambios de una rama en la rama actual.\n"
+        "```\ngit merge <nombre-rama>'\n```\n\n"
+        "Muestra el historial de commits del repositorio.\n"
+        "```\ngit log\n```\n\n"
+        "Gestiona las conexiones a repositorios remotos.\n"
+        "```\ngit remote add <nombre-remoto> <url>  # Añade un nuevo repositorio\n```\n"
+        "```\ngit remote -v                         # Muestra los repositorios\n```\n\n"
+        "Descarga los objetos y referencias de otro repositorio.\n"
+        "```\ngit fetch <nombre-remoto>\n```\n\n"
+        "Deshace commits y cambia el estado del HEAD.\n"
+        "```\ngit reset --hard <commit>  # Restablece el repositorio al estado de \nun commit específico\n```\n\n"
+        "Aplica commits de una rama sobre otra, reescribiendo el historial.\n"
+        "```\ngit rebase <nombre-rama>\n```\n\n"
+        "Guarda temporalmente los cambios no confirmados para limpiar el directorio de trabajo.\n"
+        "```\ngit stash\n```\n"
+        "```\ngit stash pop  # Restaura los cambios guardados\n```\n\n"
+    ),
+
+    
 }
